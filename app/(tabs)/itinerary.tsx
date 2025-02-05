efd1edcaaec22277ebc3a1223d55423af1ca536f
Abdi Guled <133576011+AhmedGuled@users.noreply.github.com>
2025-02-05 18:02:52 -0500

    Added changes for Ahmed-Announcements

--- app/(tabs)/itinerary.tsx
+++ app/(tabs)/itinerary.tsx
@@ -1,21 +1,21 @@
 import { View, Text, StyleSheet } from "react-native";
 
 export default function ItineraryScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Itinerary</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Itinerary</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    text: {
-      color: "#000",
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: "#000",
+  }
 });
