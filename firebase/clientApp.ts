5877f395d9e27fc038ac28c3a2342e49486adaa1
Abdirahman Nur <corsium.spongea@gmail.com>
2025-02-15 20:47:00 -0500

    Implemented create & manage announcements views

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, initializeFirestore } from "firebase/firestore";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,7 +21,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize services
-export const db = getFirestore();
+export const db = getFirestore(app);
 
 // Initialize Firebase authentication
 // export const auth = getAuth(app);
